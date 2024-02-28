eeea8ac46b80f0f47228779612c7dc6d43e43b06
snowclipsed <harrybishnoi@gmail.com>
2024-02-28 18:48:13 -0500

    Add: data getters and setters

--- include/data.hpp
+++ include/data.hpp
@@ -10,12 +10,16 @@ class Data {
 
     /*
     This is a storage class for the MNIST data.
+    R = Row Size = 28
+    C = Column Size = 28
+    L = Size of Label = Unsigned Byte (uint8_t)
+    E = Size of enumerated label = integer value from labels
     
     */
 
-    std::vector <uint8_t> * features; // this does not include the class
-    uint8_t classlabel;
-    int enumerated_classlabel;
+    std::vector <uint8_t> * features_RC; // It is a pointer to the feature vector. Size of vector = Row Size * Column Size.
+    uint8_t class_label_L;
+    int enum_label_E;
 
 
     public:
@@ -23,6 +27,10 @@ class Data {
     void set_classlabel(uint8_t);
     void set_enumerated_classlabel(int);
     void append_features(uint8_t);
+
+    int get_feature_vector_size();
+    uint8_t get_class_label();
+    uint8_t get_enumerated_class_label();
 
 
     // getters
--- src/data.cc
+++ src/data.cc
@@ -4,15 +4,37 @@
 #include <fstream>
 
 void Data::set_features(std::vector <uint8_t> *vector){
-    features = vector;
+    features_RC = vector; // setter for feature vector
 }
+
+
 void Data::set_classlabel(uint8_t label){
-    classlabel = label;
+    class_label_L = label; // setter for class label
 }
 void Data::set_enumerated_classlabel(int label){
-    enumerated_classlabel = label;
+    enum_label_E = label; // setter for enumerated class label
 }
 
 void Data::append_features(uint8_t feature){
-    features->push_back(feature);
+    features_RC->push_back(feature); // append feature to the feature vector
 }
+
+
+int Data::get_feature_vector_size(){
+    return features_RC->size(); // return the size of the feature vector
+}
+
+uint8_t Data::get_class_label(){
+    return class_label_L; // return the class label
+}
+
+uint8_t Data::get_enumerated_class_label(){
+    return enum_label_E; // return the enumerated class label
+}
+
+std::vector<uint8_t> * Data::get_features(){
+
+    return features_RC; // return the feature vector
+
+}
+
