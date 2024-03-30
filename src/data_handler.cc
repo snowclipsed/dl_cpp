87ef0e90767f975623884670e37b261fd00f20a6
snowclipsed <harrybishnoi@gmail.com>
2024-03-30 19:17:29 -0400

    Dataloader works!!

--- src/data_handler.cc
+++ src/data_handler.cc
@@ -50,7 +50,6 @@ void data_handler::load_feature_vectors(std::string PATH){
             for(int j=0; j<img_size; j++){ // iterate over pixel data after the header
                 if(fread(pixel, sizeof(pixel), 1, file)){ // read pixel data serially from the file stream into pixel[0]
                     d->append_features(pixel[0]); // append the pixel to the feature vector
-                    printf("Read pixel as %d ", pixel[0]);
                 }
                 else{
                     printf("Error reading pixel data from file\n");
@@ -58,11 +57,12 @@ void data_handler::load_feature_vectors(std::string PATH){
                 }
             }
             data->push_back(d); // we then push the data object into the data vector
+            printf("Read pixels for image %d \n", i+1);
         }
         printf("Read all data and stored in %lu", data->size());
     }  
     else{
-        printf("Error opening file\n");
+        printf("Error opening first file\n");
         exit(1);
     }
 }
@@ -82,74 +82,75 @@ void data_handler::load_feature_labels(std::string PATH){
             }
         }
         printf("Label File header obtained: %d %d\n", header[0], header[1]);
-            for(int i=0; i<header[1]; i++){
+        printf("data size: %lu\n", data->size());
+            for(int i=0; i<data->size(); i++){
                 uint8_t label[1];
                 if(fread(label, sizeof(label), 1, file)){
                         data->at(i)->set_classlabel(label[0]);
-                        printf("Read label as %d ", label[0]);
+                        printf("Read label %d as %d \n", i+1, label[0]);
                 }else{
                     printf("Error reading label data from file\n");
                     exit(1);
                 }
             }
-            printf("Read all labels and stored in %lu", data->size());
+            printf("Read all labels and stored in %lu \n", data->size());
         }else{
         printf("Error opening file\n");
         exit(1);
         }
 }
 void data_handler::split_data(){
-std::unordered_set<int> used_index;
-int train_size = data->size() * TRAINSET_PERCENT;
-int test_size = data->size() * TESTSET_PERCENT;
-int validation_size = data->size() * VALIDATIONSET_PERCENT;
+    std::unordered_set<int> used_index;
+    int train_size = data->size() * TRAINSET_PERCENT;
+    int test_size = data->size() * TESTSET_PERCENT;
+    int validation_size = data->size() * VALIDATIONSET_PERCENT;
 
-int count = 0;
-while(count<train_size){
+    int count = 0;
+    while(count<train_size){
 
-    int rand_index = rand() * data->size();
-    if(used_index.find(rand_index) == used_index.end()) // check if the find function returns the end of the set                                                        
-                                                        //if yes then the index is not in the set
-    {
-        train->push_back(data->at(rand_index));
-        used_index.insert(rand_index);
-        count++;
-    }else{continue;}
+        int rand_index = rand() % data->size();
+        if(used_index.find(rand_index) == used_index.end()) // check if the find function returns the end of the set                                                        
+                                                            //if yes then the index is not in the set
+        {
+            train->push_back(data->at(rand_index));
+            used_index.insert(rand_index);
+            count++;
+        }else{continue;}
+        }
+
+    printf("Successfully split the data into train set\n");
+    printf("Train size: %lu \n", train->size());
+
+    count = 0;
+    while(count<test_size){
+
+        int rand_index = rand() % data->size();
+        if(used_index.find(rand_index) == used_index.end()) // check if the find function returns the end of the set                                                        
+                                                            //if yes then the index is not in the set
+        {
+            test->push_back(data->at(rand_index));
+            used_index.insert(rand_index);
+            count++;
+        }else{continue;}
     }
 
-printf("Successfully split the data into train set\n");
-printf("Train size: %lu", train->size());
+    printf("Successfully split the data into test set\n");
+    printf("Test size: %lu \n", test->size());
 
-count = 0;
-while(count<test_size){
+    count = 0;
+    while(count<validation_size){
 
-    int rand_index = rand() * data->size();
-    if(used_index.find(rand_index) == used_index.end()) // check if the find function returns the end of the set                                                        
-                                                        //if yes then the index is not in the set
-    {
-        test->push_back(data->at(rand_index));
-        used_index.insert(rand_index);
-        count++;
-    }else{continue;}
-}
-
-printf("Successfully split the data into test set\n");
-printf("Test size: %lu", test->size());
-
-count = 0;
-while(count<validation_size){
-
-    int rand_index = rand() * data->size();
-    if(used_index.find(rand_index) == used_index.end()) // check if the find function returns the end of the set                                                        
-                                                        //if yes then the index is not in the set
-    {
-        validation->push_back(data->at(rand_index));
-        used_index.insert(rand_index);
-        count++;
-    }else{continue;}
-}
-printf("Successfully split the data into validation set\n");
-printf("Validation size: %lu", validation->size());
+        int rand_index = rand() % data->size();
+        if(used_index.find(rand_index) == used_index.end()) // check if the find function returns the end of the set                                                        
+                                                            //if yes then the index is not in the set
+        {
+            validation->push_back(data->at(rand_index));
+            used_index.insert(rand_index);
+            count++;
+        }else{continue;}
+    }
+    printf("Successfully split the data into validation set\n");
+    printf("Validation size: %lu \n", validation->size());
         
 }
 void data_handler::class_counter(){
@@ -178,3 +179,14 @@ std::vector <Data *> data_handler::get_test(){
 std::vector <Data *> data_handler::get_validation(){
     return *validation;
 }
+
+int main(){
+    data_handler *dh = new data_handler();
+    dh->load_feature_vectors("/home/snow/learn/dl_cpp/data/train-images.idx3-ubyte");
+    dh->load_feature_labels("/home/snow/learn/dl_cpp/data/train-labels.idx1-ubyte");
+    dh->split_data();
+    dh->class_counter();
+}
+
+// g++ -std=c++11 -I./include/ -o main ./src/* to compile main!!
+// ./main to run the program !!
