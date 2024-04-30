8553651ce26087ffdb565eb273d86185ce99ceec
snowclipsed <harrybishnoi@gmail.com>
2024-04-30 19:46:15 -0400

    Moved everything to mlp.cc file, Added random double value generator, Added network init function

---
+++ src/mlp.cc
@@ -0,0 +1,59 @@
+#include "data_handler.hpp"
+#include "data.hpp"
+#include <cstdlib>
+#include <vector>
+#include "stdio.h"
+#include "stdint.h"
+#include <ctime>
+
+
+#define INPUT_DIM 784
+#define OUTPUT_DIM 10
+#define HIDDEN_LAYER_SIZE 256
+#define NUM_HIDDEN_LAYERS 3
+
+
+class mlp{
+
+    typedef struct{
+        double* weights;
+        double* biases;
+        double* activations;
+        int num_weights;
+        
+    }network_params;
+    
+    public:
+        mlp();
+        ~mlp();
+        network_params* params;
+        network_params* init_network();
+        double random_double();
+};
+
+
+double mlp::random_double(){
+    srand(time(0));
+    double rnum = (double)rand() / RAND_MAX;
+    return rnum; // return a random double value between 0 and 1
+}
+
+mlp::network_params* mlp::init_network(){
+    /**
+     * To write an init function first we need to allocate memory for the weights using malloc.
+    */
+
+    params->num_weights = INPUT_DIM + HIDDEN_LAYER_SIZE*NUM_HIDDEN_LAYERS + OUTPUT_DIM;
+
+    params->weights = static_cast<double*>(malloc(sizeof(double) * params->num_weights));
+    params->biases = static_cast<double*>(malloc(sizeof(double) * params->num_weights));
+    params->activations = static_cast<double*>(malloc(sizeof(double) * params->num_weights));
+    
+    for(int i = 0; i < params->num_weights ; i++){
+        params->weights[i] = random_double();
+        params->biases[i] = random_double();
+        params->activations[i] = 0.00;
+    }
+   return params;
+}
+
