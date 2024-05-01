58a524a712983c865f5de333034f9a95ed33aa25
snowclipsed <harrybishnoi@gmail.com>
2024-05-01 17:34:14 -0400

    Added ReLU activation function, Added matrix multiplication function.

--- cpp_mlp/src/mlp.cc
+++ cpp_mlp/src/mlp.cc
@@ -6,8 +6,8 @@
 #include "stdint.h"
 #include <cmath>
 #include <ctime>
-
-
+#include <string>
+#include <algorithm>
 #define INPUT_DIM 784
 #define OUTPUT_DIM 10
 #define HIDDEN_LAYER_SIZE 256
@@ -31,7 +31,9 @@ class mlp{
         network_params* init_network();
         double random_double();
         double sigmoid_activation(double x);
+        double relu_activation(double x);
         void forward_pass();
+        std::vector<double> mat_mul(std::vector<double> in, std::vector<double> weights, std::vector<double> biases, std::vector<double>out, int activation);
 };
 
 mlp::mlp(){
@@ -46,7 +48,7 @@ mlp::~mlp(){
 double mlp::random_double(){
     srand(time(0));
     double rnum = (double)rand() / RAND_MAX;
-    return rnum; // return a random double value between 0 and 1
+    return rnum; // return a randoconst std::string& activationm double value between 0 and 1
 }
 
 mlp::network_params* mlp::init_network(){
@@ -54,12 +56,12 @@ mlp::network_params* mlp::init_network(){
      * To write an init function first we need to allocate memory for the weights using malloc.
     */
 
-    params->num_weights = INPUT_DIM + HIDDEN_LAYER_SIZE*NUM_HIDDEN_LAYERS + OUTPUT_DIM;
+    params->num_weights = INPUT_DIM * HIDDEN_LAYER_SIZE + HIDDEN_LAYER_SIZE*HIDDEN_LAYER_SIZE*NUM_HIDDEN_LAYERS + HIDDEN_LAYER_SIZE*OUTPUT_DIM;
     
     params->weights = static_cast<double*>(malloc(sizeof(double) * params->num_weights));
     params->biases = static_cast<double*>(malloc(sizeof(double) * params->num_weights));
     params->activations = static_cast<double*>(malloc(sizeof(double) * params->num_weights));
-    
+    float total_mem = sizeof(params->num_weights) + sizeof(params->weights) + sizeof(params->biases) + sizeof(params->activations);
     int count = 0;
 
     for(int i = 0; i < params->num_weights ; i++){
@@ -74,7 +76,8 @@ mlp::network_params* mlp::init_network(){
     if(count == params->num_weights){
         printf("Successfully initialized all neurons. \n");
         printf("Total neurons initialized : %d \n", count);
-    printf("Total number of neurons should be : %d \n", params->num_weights);
+        printf("Total number of neurons should be : %d \n", params->num_weights);
+        printf("Total memory allocated in bytes : %f \n", total_mem);
     }
 
    return params;
@@ -88,8 +91,54 @@ double mlp::sigmoid_activation(double x){
     return x;
 }
 
+double mlp::relu_activation(double x){
+    return std::max(0.00, x);
+}
+
+
+
+
+std::vector<double> mlp::mat_mul(std::vector<double> X, std::vector<double> W, std::vector<double> B, std::vector<double> Z, int activation = 1){
+/**
+ * X = Input matrix 
+ * Z = Output matrix
+ * W = Weight matrix (Size = X * Z)
+ * B = Bias matrix 
+ * forward pass eqn is => Z = W * X + B
+ * 
+ * X = [X1,X2,X3,X4,X5]
+ * Z = [A1,A2,A3,A4]
+ * W = [W1,W2,W3,W4,W5...W20]
+ * B = [B1, B2, B3, B4]
+ * 
+ * activation:
+ *  0 = no activation
+ *  1 = sigmoid
+ *  2 = ReLU
+ *
+*/
+
+    for (int i = 0; i<X.size(); i++){
+        for(int j =0; i<Z.size(); i++){
+            
+            switch(activation){
+                case 0:
+                    Z[j] = X[i+j] * B[i];
+                case 1:
+                    Z[j] = sigmoid_activation(X[i+j]*B[i]);
+                case 2:
+                    Z[j] = relu_activation(X[i+j]*B[i]);
+                    
+            }
+        }
+
+    }
+return Z;
+}
+
 
 void mlp::forward_pass(){
+
 
 }
 
