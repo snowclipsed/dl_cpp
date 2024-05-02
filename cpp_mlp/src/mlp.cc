f456d7cbee2af8a716a7d19eea1ee6c8fe625aee
snowclipsed <harrybishnoi@gmail.com>
2024-05-02 15:19:18 -0400

    Fixed matmul function, Added loguru logging instead of print statements.

--- cpp_mlp/src/mlp.cc
+++ cpp_mlp/src/mlp.cc
@@ -8,6 +8,8 @@
 #include <ctime>
 #include <string>
 #include <algorithm>
+#include "loguru.cpp"
+#include "loguru.hpp"
 #define INPUT_DIM 784
 #define OUTPUT_DIM 10
 #define HIDDEN_LAYER_SIZE 256
@@ -56,7 +58,7 @@ mlp::network_params* mlp::init_network(){
      * To write an init function first we need to allocate memory for the weights using malloc.
     */
 
-    params->num_weights = INPUT_DIM * HIDDEN_LAYER_SIZE + HIDDEN_LAYER_SIZE*HIDDEN_LAYER_SIZE*NUM_HIDDEN_LAYERS + HIDDEN_LAYER_SIZE*OUTPUT_DIM;
+    params->num_weights = INPUT_DIM * HIDDEN_LAYER_SIZE + HIDDEN_LAYER_SIZE*HIDDEN_LAYER_SIZE + HIDDEN_LAYER_SIZE*OUTPUT_DIM;
     
     params->weights = static_cast<double*>(malloc(sizeof(double) * params->num_weights));
     params->biases = static_cast<double*>(malloc(sizeof(double) * params->num_weights));
@@ -68,16 +70,16 @@ mlp::network_params* mlp::init_network(){
         params->weights[i] = random_double();
         params->biases[i] = random_double();
         params->activations[i] = 0.00;
-        // printf("Initialized neuron : %d \n", i);
+        // RAW_LOG_F(0, "Initializing Neuron");
         count++;
     }
 
 
     if(count == params->num_weights){
-        printf("Successfully initialized all neurons. \n");
-        printf("Total neurons initialized : %d \n", count);
-        printf("Total number of neurons should be : %d \n", params->num_weights);
-        printf("Total memory allocated in bytes : %f \n", total_mem);
+        LOG_F(0, "Successfully initialized all weights with random values.");
+        LOG_F(0, "Total weights initialized : %d", count);
+        LOG_F(0, "Total number of weights should be : %d", params->num_weights);
+        LOG_F(0, "Total memory allocated for weights and biases is %f Bytes", total_mem);
     }
 
    return params;
@@ -87,7 +89,7 @@ mlp::network_params* mlp::init_network(){
 double mlp::sigmoid_activation(double x){
 
     x = 1 / (1 + exp(x * -1));
-    printf("Activation is : %f \n", x);
+    // printf("activation: %f", x);
     return x;
 }
 
@@ -98,11 +100,11 @@ double mlp::relu_activation(double x){
 
 
 
-std::vector<double> mlp::mat_mul(std::vector<double> X, std::vector<double> W, std::vector<double> B, std::vector<double> Z, int activation = 1){
+std::vector<double> mlp::mat_mul(std::vector<double> X, std::vector<double> W, std::vector<double> B, std::vector<double> Z, int activation){
 /**
  * X = Input matrix 
  * Z = Output matrix
- * W = Weight matrix (Size = X * Z)
+ * W = Weight matrix (Size = X * Z) 784 * 256
  * B = Bias matrix 
  * forward pass eqn is => Z = W * X + B
  * 
@@ -115,23 +117,23 @@ std::vector<double> mlp::mat_mul(std::vector<double> X, std::vector<double> W, s
  *  0 = no activation
  *  1 = sigmoid
  *  2 = ReLU
+ * 
  *
 */
-
-    for (int i = 0; i<X.size(); i++){
-        for(int j =0; i<Z.size(); i++){
-            
-            switch(activation){
-                case 0:
-                    Z[j] = X[i+j] * B[i];
-                case 1:
-                    Z[j] = sigmoid_activation(X[i+j]*B[i]);
-                case 2:
-                    Z[j] = relu_activation(X[i+j]*B[i]);
-                    
-            }
+    for (int i = 0; i<Z.size(); i++){
+        double sum = 0.0;
+        for(int j =0; j<X.size(); j++){
+            sum += X[j]*W[X.size()*i+j];
         }
+        switch(activation){
 
+        case 1:
+            Z[i] = sigmoid_activation(sum + B[i]);
+        case 2:
+            Z[i] = relu_activation(sum + B[i]);
+        default:
+            Z[i] = sigmoid_activation(sum + B[i]);
+        }
     }
 return Z;
 }
@@ -146,4 +148,12 @@ void mlp::forward_pass(){
 int main(){
     mlp *nn = new mlp();
     nn->init_network();
+
+    std::vector<double> X = {1.0,2.0,3.0,4.0};
+    std::vector<double> W = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0};
+    std::vector<double> B = {0.0,0.0,0.0};
+    std::vector<double> Z = {0.0,0.0,0.0};
+    nn->mat_mul(X,W,B,Z,0);
+
+
 }
