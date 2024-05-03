e7bc143b377c23187a8f4d3cdfd9b0cc15895595
snowclipsed <harrybishnoi@gmail.com>
2024-05-02 21:08:47 -0400

    Refactor: Changed initialization of weights bias and activation pointer arrays to pointer vectors.

--- cpp_mlp/src/mlp.cc
+++ cpp_mlp/src/mlp.cc
@@ -11,6 +11,7 @@
 #include <algorithm>
 #include "loguru.cpp"
 #include "loguru.hpp"
+
 #define INPUT_DIM 784
 #define OUTPUT_DIM 10
 #define HIDDEN_LAYER_SIZE 256
@@ -20,22 +21,23 @@
 class mlp{
 
     typedef struct{
-        double* weights;
-        double* biases;
-        double* activations;
+        std::vector<double*> weights;
+        std::vector<double*> biases;
+        std::vector<double*> activations;
+        int num_activations;
         int num_weights;
-        
+        int num_biases;
     }network_params;
     
     public:
         mlp();
         ~mlp();
         network_params* params;
-        network_params* init_network();
-        double random_double();
+        network_params* init_network(network_params* params);
+        double* random_double();
         double sigmoid_activation(double x);
         double relu_activation(double x);
-        void forward_pass();
+        void forward_pass(network_params* params);
         std::vector<double> mat_mul(std::vector<double> in, std::vector<double> weights, std::vector<double> biases, std::vector<double>out, int activation);
 };
 
@@ -48,39 +50,46 @@ mlp::~mlp(){
 }
 
 
-double mlp::random_double(){
+double* mlp::random_double(){
     srand(time(0));
-    double rnum = (double)rand() / RAND_MAX;
+    double* rnum = new double; 
+    *rnum = static_cast<double>(rand()) / RAND_MAX;
     return rnum; // return a randoconst std::string& activationm double value between 0 and 1
 }
 
-mlp::network_params* mlp::init_network(){
+mlp::network_params* mlp::init_network(network_params* params){
     /**
      * To write an init function first we need to allocate memory for the weights using malloc.
     */
 
-    params->num_weights = INPUT_DIM * HIDDEN_LAYER_SIZE + HIDDEN_LAYER_SIZE*HIDDEN_LAYER_SIZE + HIDDEN_LAYER_SIZE*OUTPUT_DIM;
-    
-    params->weights = static_cast<double*>(malloc(sizeof(double) * params->num_weights));
-    params->biases = static_cast<double*>(malloc(sizeof(double) * params->num_weights));
-    params->activations = static_cast<double*>(malloc(sizeof(double) * params->num_weights));
-    float total_mem = sizeof(params->num_weights) + sizeof(params->weights) + sizeof(params->biases) + sizeof(params->activations);
+    params->num_weights = INPUT_DIM * HIDDEN_LAYER_SIZE + HIDDEN_LAYER_SIZE*HIDDEN_LAYER_SIZE*(NUM_HIDDEN_LAYERS-1) + HIDDEN_LAYER_SIZE*OUTPUT_DIM;
+    params->num_activations = HIDDEN_LAYER_SIZE*NUM_HIDDEN_LAYERS + OUTPUT_DIM;
+    params->num_biases = HIDDEN_LAYER_SIZE*NUM_HIDDEN_LAYERS + OUTPUT_DIM;
+    params->weights.resize(params->num_weights);
+    params->biases.resize(params->num_biases);
+    params->activations.resize(params->num_activations);
+    LOG_F(0, "Length of activations vector %ld", params->activations.size());
     int count = 0;
 
     for(int i = 0; i < params->num_weights ; i++){
         params->weights[i] = random_double();
-        params->biases[i] = random_double();
-        params->activations[i] = 0.00;
-        // RAW_LOG_F(0, "Initializing Neuron");
         count++;
     }
+    LOG_F(0, "Successfully initialized all weights with random values.");
 
+    for(int i = 0; i < params -> num_activations; i++){
+        params->biases[i] = random_double();
+        double* zero = new double; 
+        *zero = 0.000;
+        params->activations[i]= zero;   
+    }
+    LOG_F(0, "Successfully initialized all biases with random values.");
+    LOG_F(0, "Successfully initialized all activations to zero.");
 
     if(count == params->num_weights){
-        LOG_F(0, "Successfully initialized all weights with random values.");
+        
         LOG_F(0, "Total weights initialized : %d", count);
         LOG_F(0, "Total number of weights should be : %d", params->num_weights);
-        LOG_F(0, "Total memory allocated for weights and biases is %f Bytes", total_mem);
     }
 
    return params;
@@ -130,20 +139,34 @@ std::vector<double> mlp::mat_mul(std::vector<double> X, std::vector<double> W, s
 
         case 1:
             Z[i] = sigmoid_activation(sum + B[i]);
+            LOG_F(0, "Using sigmoid activation.");
         case 2:
             Z[i] = relu_activation(sum + B[i]);
+            LOG_F(0, "Using ReLU activation.");
         default:
             Z[i] = sigmoid_activation(sum + B[i]);
+            LOG_F(0, "Using sigmoid activation.");
         }
     }
 return Z;
 }
 
 
-void mlp::forward_pass(){
-    /*
-     * 
+void mlp::forward_pass(network_params* params){
+    /**
+     * Forward pass takes input image vector and calculates the values for the activations for different layers.
+     * First we calculate the weights and activations for the first hidden layer, then we use those activations for second hidden layer, 
+     * and then the third activation for output layer
     */
+
+   //Between input layer and first hidden layer
+
+    // Z = first hidden, X = input, W = weights
+
+    
+
+
+    // for (int i = 0; i<NUM_HIDDEN_LAYERS; i++)
 
 }
 
@@ -157,13 +180,13 @@ int main(){
     dh->class_counter();
 
     mlp *nn = new mlp();
-    nn->init_network();
+    nn->init_network(nn->params);
 
-    std::vector<double> X = {1.0,2.0,3.0,4.0};
-    std::vector<double> W = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0};
-    std::vector<double> B = {0.0,0.0,0.0};
-    std::vector<double> Z = {0.0,0.0,0.0};
-    nn->mat_mul(X,W,B,Z,0);
+    // std::vector<double> X = {1.0,2.0,3.0,4.0};
+    // std::vector<double> W = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0};
+    // std::vector<double> B = {0.0,0.0,0.0};
+    // std::vector<double> Z = {0.0,0.0,0.0};
+    // nn->mat_mul(X,W,B,Z,0);
 
 
 }
