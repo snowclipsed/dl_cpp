8b28af85326dffab899b7ed37efaa450b06a7b4f
snowclipsed <harrybishnoi@gmail.com>
2024-05-13 16:46:32 -0400

    Added a batching function.

--- cpp_mlp/src/mlp.cc
+++ cpp_mlp/src/mlp.cc
@@ -8,6 +8,7 @@
 #include <cmath>
 #include <ctime>
 #include <string>
+#include <random>
 #include <algorithm>
 #include "loguru.cpp"
 #include "loguru.hpp"
@@ -16,6 +17,7 @@
 #define OUTPUT_DIM 10
 #define HIDDEN_LAYER_SIZE 256
 #define NUM_HIDDEN_LAYERS 3
+#define BATCH_SIZE 16
 
 
 class mlp{
@@ -34,13 +36,18 @@ class mlp{
     public:
         mlp();
         ~mlp();
+        
         network_params* params;
         network_params* init_network(network_params* params, std::vector<Data*> train);
+        
         double* random_double();
         double sigmoid_activation(double x);
         double relu_activation(double x);
+        
         void forward_pass(network_params* params, std::vector<Data*> train);
+        
         void create_one_hot(network_params* params, std::vector<Data*> train, int num_classes);
+        std::vector<Data*> create_batch(std::vector<Data*> data);
         std::vector<double*> mat_mul(std::vector<double*> X, int X_start, int X_end, std::vector<double*> W, int W_start, int W_end, std::vector<double*> B, std::vector<double*> Z, int Z_start, int Z_end, int activation);
 };
 
@@ -177,18 +184,39 @@ std::vector<double*> convertVector(const std::vector<uint8_t>* input) {
     return output;
 }
 
+std::vector<Data*> mlp::create_batch(std::vector<Data*> data){
+    std::vector<Data*> batch;
+    batch.reserve(BATCH_SIZE);
 
+    // Create a random number generator and shuffler
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    // Shuffle the input data vector
+    std::vector<Data*> shuffled_data = data;
+    std::shuffle(shuffled_data.begin(), shuffled_data.end(), gen);
+
+    // Select the first batch_size elements from the shuffled vector
+    for (int i = 0; i < BATCH_SIZE; ++i) {
+        batch.push_back(shuffled_data[i]);
+    }
+    return batch;
+}
+
+
+/**
+ * @brief Forward pass takes input image vector and calculates the values for the activations for different layers.
+ * First we calculate the weights and activations for the first hidden layer, then we use those activations for second hidden layer, 
+ * and then the third activation for output layer
+*/
 void mlp::forward_pass(network_params* params, std::vector<Data*> train){
-    /**
-     * Forward pass takes input image vector and calculates the values for the activations for different layers.
-     * First we calculate the weights and activations for the first hidden layer, then we use those activations for second hidden layer, 
-     * and then the third activation for output layer
-    */
 
-   //Between input layer and first hidden layer
+
+    //Between input layer and first hidden layer
 
     // Z = first hidden, X = input, W = weights
-    
+    LOG_F(0, "Initializing forward pass.");
+
     for(int i = 0; i<train.size(); i++){
         // initially we put in the input vector of size 784 into the first hidden layer of size 256.
         int X_start = 0;
@@ -225,18 +253,48 @@ void mlp::forward_pass(network_params* params, std::vector<Data*> train){
             // LOG_F(0, "%d, %d, %d, %d, %d, %d", X_start, X_end, Z_start, Z_end, W_start, W_end);
             mat_mul(params->activations, X_start, X_end, params->weights, W_start, W_end, params->biases, params->activations, Z_start, Z_end, 1);
             params->pred.insert(params->pred.end(), params->activations.end()-10, params->activations.end());
-    LOG_F(0, "Forward pass for image : %d", i);
+        
+            LOG_F(0, "Forward pass for image : %d", i);
     }
 
-LOG_F(0, "Forward pass completed.");
+    LOG_F(0, "Forward pass completed.");
 }
 
+
+/**
+ * @brief Creates a one-hot vector representation of the class labels for the given training data.
+ *
+ * This function iterates over the training data and constructs a one-hot vector representation
+ * of the class labels. For each data instance, a vector of length `num_classes` is created,
+ * where all elements are set to 0, except for the element corresponding to the class label,
+ * which is set to 1.
+ *
+ * The one-hot vectors are stored in the `true_pred` vector of the `network_params` object,
+ * where each element is a pointer to a `double` value representing the corresponding element
+ * in the one-hot vector.
+ *
+ * @param params Pointer to the `network_params` object containing the `true_pred` vector.
+ * @param train Vector of pointers to `Data` objects representing the training data.
+ * @param num_classes Number of classes in the training data.
+ *
+ * @throw std::exception (or any derived exception) if any error occurs during the operation.
+ *
+ * @note The function dynamically allocates memory for the `double` pointers stored in the
+ *       `true_pred` vector. It is the responsibility of the caller to ensure proper memory
+ *       management and deallocation of these pointers when they are no longer needed.
+ */
 void mlp::create_one_hot(network_params* params, std::vector<Data*> train, int num_classes){
+    /**
+     * 
+    */
+
+    LOG_F(0, "Creating one hot vector.");
     
+
     for(int image = 0; image<train.size(); image++){
         for(int classlabel = 0 ; classlabel < num_classes; classlabel++){
             if (train[image]->get_enumerated_class_label() == classlabel){
-                double labelvalue = static_cast<double>(train[image]->get_enumerated_class_label());
+                double labelvalue = 1.00;
                 double* label_ptr = new double(labelvalue);
                 params->true_pred.push_back(label_ptr);
             }else{
@@ -245,14 +303,15 @@ void mlp::create_one_hot(network_params* params, std::vector<Data*> train, int n
                 params->true_pred.push_back(label_ptr);
             }
         }
-        LOG_F(0, "One hot vector created for image %d", image);
+        // LOG_F(0, "One hot vector created for image %d", image);
     }
 }
 
-double cross_entropy_loss(std::vector<double*> pred, std::vector<int> label){
+double cross_entropy_loss(std::vector<double*> pred, std::vector<double*> true_pred){
     double loss = 0.0;
 
-    for(int i = 0; i < label.size(); i++){
+    for(int i = 0; i < true_pred.size(); i++){
+        
         
     }
 
@@ -275,9 +334,13 @@ int main(){
 
     mlp *nn = new mlp();
     nn->init_network(nn->params, dh->get_train());
-    nn->create_one_hot(nn->params, dh->get_train(), num_classes);
-    nn->forward_pass(nn->params, dh->get_train());
-    LOG_F(0, "%ld", dh->get_train().size());
-    LOG_F(0, "%ld", nn->params->pred.size());
+
+    std::vector<Data*> batch = nn->create_batch(dh->get_train());
+    
+    nn->create_one_hot(nn->params, batch, num_classes);
+    nn->forward_pass(nn->params, batch);
+    LOG_F(0, "Train size = %ld", dh->get_train().size());
+    LOG_F(0, "Pred size = %ld", nn->params->pred.size());
+    LOG_F(0, "True Pred size = %ld", nn->params->true_pred.size());
 
 }
