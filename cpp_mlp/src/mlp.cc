3b84a594130190286446419884d3db84e1637bc0
snowclipsed <harrybishnoi@gmail.com>
2024-05-28 14:45:29 -0400

    Experimenting with imgui

--- cpp_mlp/src/mlp.cc
+++ cpp_mlp/src/mlp.cc
@@ -12,6 +12,31 @@
 #include <algorithm>
 #include "loguru.cpp"
 #include "loguru.hpp"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#include <future>
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
+#ifdef __EMSCRIPTEN__
+#include "../libs/emscripten/emscripten_mainloop_stub.h"
+#endif
+
+
+
 
 #define INPUT_DIM 784
 #define OUTPUT_DIM 10
@@ -586,10 +611,29 @@ void mlp::backward_pass(network_params* params, std::vector<Data*> batch){
     }
 }
 
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
 
-int main(){
 
-    data_handler *dh = new data_handler();
+double run_mlp(data_handler* dh, mlp* nn, int num_classes){
+
+std::vector<Data*> batch = nn->create_batch(dh->get_train());
+
+nn->create_one_hot(nn->params, batch, num_classes);
+nn->forward_pass(nn->params, batch);
+nn->backward_pass(nn->params, batch);
+double CE_loss = cross_entropy_loss(nn->params->pred, nn->params->true_pred);
+return CE_loss;
+}
+
+
+
+
+int main(int, char**){
+
+        data_handler *dh = new data_handler();
     dh->load_feature_vectors("/home/snow/learn/dl_cpp/cpp_mlp/dataset/train-images-idx3-ubyte");
     dh->load_feature_labels("/home/snow/learn/dl_cpp/cpp_mlp/dataset/train-labels-idx1-ubyte");
     dh->split_data();
@@ -598,23 +642,123 @@ int main(){
 
     mlp *nn = new mlp();
     nn->init_network(nn->params, dh->get_train());
-    
     LOG_F(0, "Train size = %ld", dh->get_train().size());
     int num_batches = dh->get_train().size()/BATCH_SIZE;
 
-    for(int batch_num = 0; batch_num < num_batches; batch_num++){
-        std::vector<Data*> batch = nn->create_batch(dh->get_train());
+    std::future<double> loss;
+    double last_loss;
 
-        nn->create_one_hot(nn->params, batch, num_classes);
-        nn->forward_pass(nn->params, batch);
-        LOG_F(0, "Forward pass completed for batch %d.", batch_num);
-        nn->backward_pass(nn->params, batch);
-        
-        LOG_F(0, "Backward pass completed for batch %d.", batch_num);
-        double CE_loss = cross_entropy_loss(nn->params->pred, nn->params->true_pred);
-        LOG_F(0, "Loss : %f", CE_loss);
-  
-    }
+    glfwSetErrorCallback(glfw_error_callback);
+        if (!glfwInit())
+            return 1;
 
+    // Decide GL+GLSL versions
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+
+
+        // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+#ifdef __EMSCRIPTEN__
+    ImGui_ImplGlfw_InstallEmscriptenCanvasResizeCallback("#canvas");
+#endif
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Our state
+    bool show_demo_window = true;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+        // Main loop
+    #ifdef __EMSCRIPTEN__
+        // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
+        // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
+        io.IniFilename = nullptr;
+        EMSCRIPTEN_MAINLOOP_BEGIN
+    #else
+        while (!glfwWindowShouldClose(window))
+    #endif
+        {
+            // Poll and handle events (inputs, window resize, etc.)
+            // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+            // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+            // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+            // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+            glfwPollEvents();
+
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+            if (show_demo_window)
+                ImGui::ShowDemoWindow(&show_demo_window);
+
+            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+            {
+
+                ImGui::Begin("MLP");
+
+                if (ImGui::Button("Run MLP")){
+                        for(int batch_num = 0; batch_num < num_batches; batch_num++){
+                            loss = std::async(std::launch::async, run_mlp, dh, nn, num_classes);
+                            
+                            if(loss.valid()){
+                                if(loss.wait_for(std::chrono::milliseconds(1)) == std::future_status::ready){
+                                last_loss = loss.get();
+                                }
+                            }
+                        LOG_F(0,"Loss : %f",last_loss);
+                        }
+                }
+                ImGui::End();
+            }
+
+            // Rendering
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glfwSwapBuffers(window);
+        }
+    #ifdef __EMSCRIPTEN__
+        EMSCRIPTEN_MAINLOOP_END;
+    #endif
+
+        // Cleanup
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
+
+        return 0;
 
 }
